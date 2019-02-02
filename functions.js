1fde9dd49826f6e10b8179dd85926cc507e25786
bera5186 <rbasu611@gmail.com>
2019-02-02 15:55:50 +0530

    Function Expressions

--- functions.js
+++ functions.js
@@ -27,27 +27,53 @@ var occu = function(job, firstName) {
         case 'driver' :  return firstName + 'drives cars';
         default : firstName + 'does something else !! ';
     }
-}
+};
 console.log(occu('driver', 'Rahul'));
 
+// method invocation pattern
+var myObject = {
+    value: 0,
+    increment: function(inc){
+        this.value += typeof inc === 'number' ? inc : 1;
+    }
+};
+myObject.increment();
+document.write(myObject.value);
 
+myObject.increment(2);
+document.write(myObject.value);
 
+var add = function(a, b){
+    return a+b;
+}
+// Function Invocation Pattern
+myObject.double = function () {
+    var that = this;
+    var helper = function () {
+        that.value = add(that.value, that.value);
+    }
+    helper();
+};
 
+myObject.double();
+window.alert(myObject.value);
 
+// Exceptions
+var tryIt = function(){
+    try {
+        add("Seven");
+    } catch (e) {
+        console.log(e.name + ' : '+e.message);
+    }
+}
+tryIt();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// function expression
+var greet  = function(name, time) {
+    if (time >= 12) {
+        console.log('Good evening '+ name);        
+    } else {
+        console.log('Good morning '+ name);
+    }
+}
+greet('rahul', 15);
